7d37f97696a3d69b15f7d05f0ce13c3fb2a01423
kpkpixels <pablobentz@gmail.com>
2023-07-06 17:03:47 -0300

    commit inicial + estrutura

---
+++ js/models/questao.js
@@ -0,0 +1,7 @@
+class Questao {
+    constructor(texto, respostas, indiceRespostaCorreta) {
+        this.texto = texto;
+        this.respostas = respostas;
+        this.indiceRespostaCorreta = indiceRespostaCorreta;
+    }
+}
